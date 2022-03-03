ade62e75cd650aac512d83bfd571588d6160a188
Caio Severino <kaiosev9@gmail.com>
2022-03-03 15:00:09 -0300

    add mais espec

--- Site/scripts/profissionais-box.js
+++ Site/scripts/profissionais-box.js
@@ -6,6 +6,12 @@ const arrow = document.querySelector('.fa-angle-down')
 const psico= document.querySelector('.prof__psico');
 const pintura= document.querySelector('.prof__pintura');
 const mass= document.querySelector('.prof__mass');
+const mod= document.querySelector('.prof__mod');
+const yoga= document.querySelector('.prof__yoga');
+const danca= document.querySelector('.prof__danca');
+const conste= document.querySelector('.prof__const');
+const acun= document.querySelector('.prof__acun');
+const canto= document.querySelector('.prof__canto');
 
 document.querySelectorAll('#opcoes > .opcao').forEach((opcao) =>{
     console.log('AAA')
@@ -27,14 +33,94 @@ function ShowSec(sec){
         psico.classList.add('show-prof')
         pintura.classList.remove('show-prof')
         mass.classList.remove('show-prof')
+        mod.classList.remove('show-prof')
+        yoga.classList.remove('show-prof')
+        danca.classList.remove('show-prof')
+        conste.classList.remove('show-prof')
+        acun.classList.remove('show-prof')
+        canto.classList.remove('show-prof')
+
     }else if(sec == "Pintura"){
         pintura.classList.add('show-prof')
         mass.classList.remove('show-prof')
         psico.classList.remove('show-prof')
+        mod.classList.remove('show-prof')
+        yoga.classList.remove('show-prof')
+        danca.classList.remove('show-prof')
+        conste.classList.remove('show-prof')
+        acun.classList.remove('show-prof')
+        canto.classList.remove('show-prof')
+
     }else if(sec =="Massagem"){
         mass.classList.add('show-prof')
         pintura.classList.remove('show-prof')
         psico.classList.remove('show-prof')
+        mod.classList.remove('show-prof')
+        yoga.classList.remove('show-prof')
+        danca.classList.remove('show-prof')
+        conste.classList.remove('show-prof')
+        acun.classList.remove('show-prof')
+        canto.classList.remove('show-prof')
+    }else if(sec =="Modelacao"){
+        mod.classList.add('show-prof')
+        mass.classList.remove('show-prof')
+        pintura.classList.remove('show-prof')
+        psico.classList.remove('show-prof')
+        yoga.classList.remove('show-prof')
+        danca.classList.remove('show-prof')
+        conste.classList.remove('show-prof')
+        acun.classList.remove('show-prof')
+        canto.classList.remove('show-prof')
+    }else if(sec == "Yoga"){
+        yoga.classList.add('show-prof')
+        mod.classList.remove('show-prof')
+        mass.classList.remove('show-prof')
+        pintura.classList.remove('show-prof')
+        psico.classList.remove('show-prof')
+        danca.classList.remove('show-prof')
+        conste.classList.remove('show-prof')
+        acun.classList.remove('show-prof')
+        canto.classList.remove('show-prof')
+    }else if(sec == "Danca"){
+        danca.classList.add('show-prof')
+        yoga.classList.remove('show-prof')
+        mod.classList.remove('show-prof')
+        mass.classList.remove('show-prof')
+        pintura.classList.remove('show-prof')
+        psico.classList.remove('show-prof')
+        conste.classList.remove('show-prof')
+        acun.classList.remove('show-prof')
+        canto.classList.remove('show-prof')
+    }else if(sec == "Constelacao"){
+        conste.classList.add('show-prof')
+        danca.classList.remove('show-prof')
+        yoga.classList.remove('show-prof')
+        mod.classList.remove('show-prof')
+        mass.classList.remove('show-prof')
+        pintura.classList.remove('show-prof')
+        psico.classList.remove('show-prof')
+        acun.classList.remove('show-prof')
+        canto.classList.remove('show-prof')
+    }else if(sec=="Acunpuntura"){
+        acun.classList.add('show-prof')
+        conste.classList.remove('show-prof')
+        danca.classList.remove('show-prof')
+        yoga.classList.remove('show-prof')
+        mod.classList.remove('show-prof')
+        mass.classList.remove('show-prof')
+        pintura.classList.remove('show-prof')
+        psico.classList.remove('show-prof')
+        canto.classList.remove('show-prof')
+    }else if (sec == "Canto"){
+        canto.classList.add('show-prof')
+        acun.classList.remove('show-prof')
+        conste.classList.remove('show-prof')
+        danca.classList.remove('show-prof')
+        yoga.classList.remove('show-prof')
+        mod.classList.remove('show-prof')
+        mass.classList.remove('show-prof')
+        pintura.classList.remove('show-prof')
+        psico.classList.remove('show-prof') 
     }
 }
 
